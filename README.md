88758984371fce2545b4b7970092b5e1ed419f7f
dragon <dima_rodin@yahoo.com>
2026-01-15 14:33:21 +0200

    start point

---
+++ README.md
@@ -0,0 +1,8 @@
+# 5G RAN Simulator
+
+This project is a high-level 5G RAN network simulator designed to demonstrate C++ system programming skills and an understanding of telecommunications protocol architecture.
+
+Technology stack
+C++: gNB and UE core logic
+
+
---
+++ mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle("5G RAN Simulator");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
